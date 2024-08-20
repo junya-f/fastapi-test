fdd2f9ebd70fbfc05915b58b7f78b564984492a9
junya-f <fujiokajunya@icloud.com>
2024-08-21 07:05:35 +0900

    crud.py作成

--- sql_app/main.py
+++ sql_app/main.py
@@ -1,7 +1,15 @@
-from fastapi import Depends, FastAPI
+from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy.orm import Session
 from fastapi.security import OAuth2PasswordBearer
 
+from . import crud, models, schemas
+from . database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
+
 app = FastAPI()
+
+
 
 oauth2_schema = OAuth2PasswordBearer(tokenUrl="token")
 
